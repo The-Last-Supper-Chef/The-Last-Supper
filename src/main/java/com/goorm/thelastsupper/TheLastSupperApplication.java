b467cfff71a751070f66a8d078f50697194ec60c
ahn_h_j <tkgkd246@gmail.com>
2025-04-17 14:14:46 +0900

    init

---
+++ src/main/java/com/goorm/thelastsupper/TheLastSupperApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.thelastsupper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLastSupperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheLastSupperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goorm/thelastsupper/TheLastSupperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.thelastsupper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheLastSupperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
