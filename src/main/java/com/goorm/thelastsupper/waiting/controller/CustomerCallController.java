527945613f27a9deff994cab21968f186acc5a85
UncleSamsun <alswns5620@naver.com>
2025-04-25 14:36:09 +0900

    feat: 고객 호출 기능 추가
    - 대기중인 고객 중 생성일 기준으로 가장 먼저인 고객 호출
    - 호출 완료 시 waitingStatus를 success로 변경
    
    Resolves: #35

--- src/main/java/com/goorm/thelastsupper/waiting/controller/CustomerCallController.java
+++ src/main/java/com/goorm/thelastsupper/waiting/controller/CustomerCallController.java
@@ -1,4 +1,27 @@
 package com.goorm.thelastsupper.waiting.controller;
 
+import com.goorm.thelastsupper.waiting.dto.WaitingResponse;
+import com.goorm.thelastsupper.waiting.service.CustomerCallService;
+import com.goorm.thelastsupper.waiting.service.CustomerWaitingService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/waiting")
 public class CustomerCallController {
+    private final CustomerCallService customerCallService;
+
+    @PostMapping("/call")
+    public ResponseEntity<WaitingResponse> callWaiting() {
+        WaitingResponse waitingResponse = customerCallService.callWaiting();
+
+        return ResponseEntity.ok(waitingResponse);
+    }
 }
