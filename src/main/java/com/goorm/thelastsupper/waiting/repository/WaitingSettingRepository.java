43e92445cd53f4e0d90bc4151b3632fd4405647b
Teddy <49386594+ebzm00@users.noreply.github.com>
2025-04-25 13:12:12 +0900

    feat : 웨이팅 대기열 마감 기능 구현 (#32)
    - 웨이팅 대기열 마감 기능 구현
    기존 매장이 마감일 경우 Error 예외 처리
    
    Co-authored-by: 김민준 <79427050+UncleSamsun@users.noreply.github.com>

--- src/main/java/com/goorm/thelastsupper/waiting/repository/WaitingSettingRepository.java
+++ src/main/java/com/goorm/thelastsupper/waiting/repository/WaitingSettingRepository.java
@@ -12,6 +12,8 @@ public interface WaitingSettingRepository extends JpaRepository<WaitingSetting, 
 
     boolean existsByRestaurant_IdAndWaitingSetCategory(String restaurantId, WaitingSetCategory category);
 
+    Optional<WaitingSetting> findFirstByRestaurant_IdOrderByIdDesc(String restaurantId);
+
     // 전체 중에서 ULID ID 내림차순으로 정렬해 첫 번째(=가장 최신) 건을 반환
     Optional<WaitingSetting> findFirstByOrderByIdDesc();
 }
