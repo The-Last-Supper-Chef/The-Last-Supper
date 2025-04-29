114d8d4a5935daf0e211bed5af004ee80d474325
hamgyusang <ebzm00@naver.com>
2025-04-29 09:35:22 +0900

    feat : 웨이팅 대기열 조회 기능 구현
    - 웨이팅 대기열 조회 기능 구현
    웨이팅 대기열 중에서 Waiting인 상태값만 조회되도록 구현
    
    Resovles: #47

---
+++ src/main/java/com/goorm/thelastsupper/waiting/controller/WaitingQueueController.java
@@ -0,0 +1,27 @@
+package com.goorm.thelastsupper.waiting.controller;
+
+import com.goorm.thelastsupper.waiting.dto.WaitingResponse;
+import com.goorm.thelastsupper.waiting.entity.WaitingQueue;
+import com.goorm.thelastsupper.waiting.service.WaitingQueueService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/waitings")
+@RequiredArgsConstructor
+@Slf4j
+public class WaitingQueueController {
+
+    private final WaitingQueueService waitingQueueService;
+
+    //대기중인 팀 리스트 조회
+    @GetMapping("/queue")
+    public List<WaitingResponse> getWaitingList() {
+        return waitingQueueService.getWaitingList();
+    }ㅂㅂㅂㅂㅂㅂㅂㅂㅂㅂ
+}
