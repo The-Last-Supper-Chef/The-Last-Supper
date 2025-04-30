78f1716346d836d6f9028d7ba1731ff10ef036da
JiELee1 <dlwldmscjstk@naver.com>
2025-04-30 15:57:44 +0900

    feat: 회원 정보 조회 기능 추가

---
+++ src/main/java/com/goorm/thelastsupper/account/controller/AccountController.java
@@ -0,0 +1,34 @@
+package com.goorm.thelastsupper.account.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.goorm.thelastsupper.account.dto.AccountResponse;
+import com.goorm.thelastsupper.account.service.AccountService;
+import com.goorm.thelastsupper.common.security.CustomPrincipal;
+import com.goorm.thelastsupper.waiting.dto.WaitingPositionResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@Validated
+public class AccountController {
+
+	private final AccountService accountService;
+
+	@GetMapping("/customers")
+	public ResponseEntity<AccountResponse> getAccount(@AuthenticationPrincipal CustomPrincipal customPrincipal) {
+		AccountResponse response = accountService.getAccount(customPrincipal.getId());
+		return ResponseEntity.ok(response);
+	}
+
+}
