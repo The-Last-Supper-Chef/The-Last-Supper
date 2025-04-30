1f6daea71552199bb5120d35ac307d7ba469100d
UncleSamsun <alswns5620@naver.com>
2025-04-30 14:53:43 +0900

    feat: 프로메테우스, 그라파나, jmeter 관련 시큐리티 설정 수정

--- src/main/java/com/goorm/thelastsupper/common/config/SecurityConfig.java
+++ src/main/java/com/goorm/thelastsupper/common/config/SecurityConfig.java
@@ -33,9 +33,9 @@ public class SecurityConfig {
 			.csrf(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.sessionManagement(sm ->
-				sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+				sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/api/v1/signup", "/api/v1/login","/api/v1/**").permitAll() //임시
+				.requestMatchers("/api/v1/signup", "/api/v1/login","/api/v1/**", "/actuator/**", "/metrics", "/prometheus").permitAll()
 				.anyRequest().authenticated()
 			)
 			.addFilterBefore(
